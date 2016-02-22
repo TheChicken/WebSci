e4a7c563c58defec2a11b61f1500d1ed59850eb3
Chelsea <valenc2@rpi.edu>
2016-02-22 16:39:04 -0500

    lab 2 plus more files

---
+++ lab2/README.md
@@ -0,0 +1,8 @@
+Chelsea Valente
+Web Science - Lab 2
+
+For this lab, I used openweather's API and HTML5 Geolocation API to create a weather forecast. I first called the Geolocation to get he user's latitude and logitude which I then used in the AJAX url to call the openweather API. I used the jsonp return format we spoke about in class which was very useful when I needed to locate the properties returned from the API. Interestingly enough, when I first started this lab and just was concerned with finidng the local weather I was able to specify units=imperial in the URL. When I switched to the forcast API, I was able to get the max and min values in fahrenheit but the current temperature came in kelvin! I am not sure why that happened, but it was interesting to notice that difference. 
+
+I had a lot of fun learning more about Bootstrap and getting more comfortable with the columns and rows. I started off a little frustrated but once I figured out how to start aligning things I really enjoyed it! I have to say that I am really proud of how this lab came out and I feel pretty accomplished! The only thing I would change is maybe dealing with the window resizing a little more gracefully. 
+
+I did use some CSS for the styling but want to experiment more with the div classes span and offsets to let the structure really do the bulk of the work.
--- lab2/lab2.js
+++ lab2/lab2.js
@@ -1,22 +1,70 @@
+$(document).ready(function(){
+    
+    navigator.geolocation.getCurrentPosition(handle_geolocation_query, handle_errors);
+
+    function handle_errors(error)
+            {
+                switch(error.code)
+                {
+                    case error.PERMISSION_DENIED: alert("user did not share geolocation data");
+                    break;
+     
+                    case error.POSITION_UNAVAILABLE: alert("could not detect current position");
+                    break;
+     
+                    case error.TIMEOUT: alert("retrieving position timed out");
+                    break;
+     
+                    default: alert("unknown error");
+                    break;
+                }
+            }
+
+    function handle_geolocation_query(position){
+
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&units=imperial&APPID=d85a72af46b6c4962441b4bae789e175",
+            method: 'GET',
+            dataType: "jsonp",
+
+            success:function(response){
+
+                console.log(response);
+
+                var fullDate = new Date();
+                console.log(fullDate);
+                var months = {'0':'Jan', '1':'Feb', '2':'Mar', '3':'Apr', '4':'May', '5':'Jun', '6':'Jul', '7':'Aug', '8':'Sep', '9':'Oct', '10':'Nov', '11':'Dec'}
+                var date = new Date();
+                var month = date.getMonth();
+                var day = date.getDate();
+
+                var temperatureF = (response.list[0].deg * (9/5) - 459.67).toFixed(0)
+
+                $('.city').html(response.city.name + ', ' +response.city.country);
+                $('.temp').html(temperatureF);
+                $('.todayDate').html(months[month] + ' ' +day);
+                $('.weatherIcon').html('<img alt="icon" id="icon" src="http://openweathermap.org/img/w/' + response.list[0].weather[0].icon + '.png"/>');
+                $('.weatherGroup').html(response.list[0].weather[0].main);
+                $('.weatherDesc').html(response.list[0].weather[0].description);
+                $('.windSpeed').html(response.list[0].speed);
+                $('.humidity').html(response.list[0].humidity);
+
+                
+
+                for(var i=1; i<=9; i++){
+                
+                    var months = {'0':'Jan', '1':'Feb', '2':'Mar', '3':'Apr', '4':'May', '5':'Jun', '6':'Jul', '7':'Aug', '8':'Sep', '9':'Oct', '10':'Nov', '11':'Dec'}
+                    var date = new Date();
+                    date.setDate(date.getDate() + i);
+                    console.log(date);
+                    var month = date.getMonth();
+                    var day = date.getDate();
 
 
-function getLocation(){
-	if(navigator.geolocation){
+                    $('.forecast').append('<div class="col-xs-1" id="forecasts"><p id="forIcon"><span class="forIcon"><p id="forWeather"><p id="date"><span class="forDate">' + months[month] + ' ' + day + '</span></p><img alt="icon" id="icon" src="http://openweathermap.org/img/w/' + response.list[i].weather[0].icon + '.png"/></span><p id="weater"><span class="weather">' + response.list[i].weather[0].main + '</span></p><p id="forMax">Max: <span class="forMax">' + (response.list[i].temp.max).toFixed(0) + '</span>&deg;F</p><p id="forMin">Min: <span class="forMin">' + (response.list[i].temp.min).toFixed(0) + '</span>&deg;F</p></div>');
+                }
+            }
+        });
 
-		navigator.geolocation.getCurrentPosition(showPosition);
-	} else{
-		$('#warning').appent("Geolocation is not supported by this browser.");
-	}
-
-	function showPosition (position){
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?lat={" + position.coords.Latitude + "}&lon={" + position.coords.Longitude + "}&APPID={d85a72af46b6c4962441b4bae789e175}", false);
-		xhr.send();
-
-		$.get("http://api.openweathermap.org/data/2.5/weather?lat={" + position.coords.Latitude + "}&lon={" + position.coords.Longitude + "}&APPID={d85a72af46b6c4962441b4bae789e175}&units=imperial", function (response){
-			
-			$('#weatherInfo').append('<li class="weather container-fluid"><div class="col-md-8 col-sm-4"><output class = "temperature">' + response.main.temp + 'F</output><details class = "description">' + response.weather[0].description + '</details>');
-		});
-	}
-
-}
+    }
+});
